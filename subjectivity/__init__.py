869fa0180d9dbf646c867f6fcbbb95ad6701df91
Adam J <amj69@case.edu>
2016-07-22 03:22:58 -0700

    lots of crazy changes. forgot to commit for a while/wasn't working too much in a directed fashion

--- subjectivity/__init__.py
+++ subjectivity/__init__.py
@@ -1,9 +1,5 @@
-from bs4 import BeautifulSoup
-import re
-import requests
-
-_common_attr = set(dir(type)) | set(['__self__', '__objclass__'])
-_subjective_map = lambda x: x if isinstance(x, Subjective) else Subjective(x)
+import bodybuilder
+from textblob import TextBlob
 
 
 class memoize(dict):
@@ -18,28 +14,88 @@ class memoize(dict):
         return result
 
 
+def objective(o):
+    if isinstance(o, Subjective):
+        return o.target
+    return o
+
+
 class Subjective(object):
-    def __init__(self, target):
-        self.target = target
-
-        valid_attrs = set(dir(target)) - _common_attr
-
-        if '__name__'
-
-        for attr in (set(dir(target)) - _common_attr - set([target.__name__])):
-            print('subjective {} {}'.format(target, attr))
-            setattr(self, attr, _subjective_map(getattr(target, attr)))
+    def __init__(self, name, target):
+        self.__dict__ = {
+            'name': name,
+            'target': target,
+        }
 
     def __call__(self, *args, **kwargs):
-        self.target(
-            map(_subjective_map, args),
-            zip(kwargs.keys(), map(_subjective_map, kwargs.values()))
-        )
+        return Subjective(self.target(*args, **kwargs))
+
+    def __getattr__(self, name):
+        return Subjective(getattr(self.target, name))
+
+    def __setattr__(self, name, value):
+        if not self.__dict__ or name in self.__dict__:
+            object.__setattr__(self, name, value)
+        else:
+            object.__setattr__(self.target, name, value)
+
+    def __int__(self):
+        return int(self.target)
+
+    def __str__(self):
+        return str(self.target)
+
+    def __repr__(self):
+        return repr(self.target)
+
+    def __blob__(self):
+        return get_basis_blob(self.name)
+
+    def __score__(self):
+        blob = self.__blob__()
+        return blob.polarity / blob.subjectivity
+
+    def __eq__(self, other):
+        if isinstance(other, Subjective):
+            return (abs(self.__score__() - other.__score__()) < 0.005)
+        return self.target == other
+
+    def __gt__(self, other):
+        if isinstance(other, Subjective):
+            return self.__score__() > other.__score__()
+        return self.target > other
+
+    def __lt__(self, other):
+        if isinstance(other, Subjective):
+            return self.__score__() < other.__score__()
+        return self.target < other
+
+    def __ge__(self, other):
+        return self > other or self == other
+
+    def __le__(self, other):
+        return self < other or self == other
+
+    def __hash__(self):
+        return self.target.__hash__()
+
+
+def similarity(a, b):
+    a = _get_as_blob(a)
+    b = _get_as_blob(b)
+
+    intersection = sum(map(a.word_counts[x] + b.word_counts[x] for x in (set(a.words) & set(b.words))))
+    union = sum(map(a.word_counts[x] + b.word_counts[x] for x in (set(a.words) | set(b.words))))
+
+    return float(intersection) / float(union)
+
+
+def _get_as_blob(term):
+    if isinstance(term, TextBlob):
+        return term
+    return get_basis_blob(term)
+
 
 @memoize
-def _scrape_results(query):
-    r = requests.get('http://www.google.com/search', params={'q': query})
-    soup = BeautifulSoup(r.text, 'lxml')
-    stats = soup.find('div', {'id': 'resultStats'}).text
-    return int(re.sub(r'\D+', '', stats))
-
+def get_basis_blob(term):
+    return bodybuilder.build_basis(term)
---
+++ subjectivity/bodybuilder.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+from google import search
+import re
+import requests
+from textblob import TextBlob
+from textblob import Word
+
+
+def sane_english(text):
+    text = text.strip().lower()
+
+    if len(text) == 0:
+        return None
+
+    sane = re.sub(r'[^a-z ]', '', text)
+    sane = re.sub(r'[ ]{2,}', ' ', sane).strip()
+
+    if float(len(sane)) / float(len(text)) < 0.85:
+        return None
+
+    good = []
+    for word in sane.split(' '):
+        if Word(word).define():
+            good.append(word)
+
+    if float(len(good)) / len(sane.split(' ')) < 0.5:
+        return None
+
+    return ' '.join(good)
+
+
+def get_top_blocks(url):
+    try:
+        blocks = BeautifulSoup(requests.get(url).text, 'lxml').get_text().split('\n')
+        blocks = filter(lambda x: x, map(lambda x: sane_english(x), blocks))
+
+        top_blocks = sorted(zip(map(lambda x: len(x), blocks), blocks))[-(len(blocks)/10):]
+        top_blocks = map(lambda x: x[1], top_blocks)
+
+        return reduce(lambda a, b: a + b, top_blocks, '')
+    except:
+        return ''
+
+
+def build_basis(term):
+    urls = list(search('wiki ' + term, stop=10))
+    return TextBlob(' '.join([get_top_blocks(url) for url in urls]))
+
